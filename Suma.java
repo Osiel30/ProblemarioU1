0d6f2d901918be44327997c9a0466b2eb8952878
Osiel30 <2230412@upv.edu.mx>
2024-01-29 10:22:04 -0600

    Suma

---
+++ Suma.java
@@ -0,0 +1,11 @@
+package edu.upvictoria.fpoo;
+
+public class Suma {
+
+    public static long realizarSuma(Number a, Number b) {
+        double resultado = a.doubleValue() + b.doubleValue();
+        System.out.println("La suma de los números es: " + resultado);
+        return 0;
+    }
+}
+
